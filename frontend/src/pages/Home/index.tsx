0fac1e62e51c1f97586beccdf840406f5179ece1
Daniel Rocha <danrochamaia@gmail.com>
2021-11-05 17:23:43 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => { //expressão lambda (atribuindo a variável Home a função sem nome)
+    return (
+        <div>
+            <h1>Página Home</h1>
+        </div>
+    );
+}
+
+export default Home;
