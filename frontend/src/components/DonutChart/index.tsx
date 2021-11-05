9154b2e309a19b56897a0fd5ed182c5a1539fc46
Daniel Rocha <danrochamaia@gmail.com>
2021-11-05 20:00:13 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,10 +1,31 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 const DonutChart = () => {
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Jim Halper', 'Dwight Schrutet', 'Stanley Hudson', 'Kelly Kapoor', 'Ryan Howard']
-    }
+
+    //FORMA ERRADA
+    let chartData : ChartData = { labels: [], series: []};
+    //()`${BASE_URL}/sales/amount-by-seller`)
+    axios.get(BASE_URL + '/sales/amount-by-seller')
+        .then(response => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+            chartData = { labels: myLabels, series: mySeries};
+            console.log(chartData);
+        });
+
+    //const mockData = {
+    //    series: [477138, 499928, 444867, 220426, 473088],
+    //    labels: ['Jim Halper', 'Dwight Schrutet', 'Stanley Hudson', 'Kelly Kapoor', 'Ryan Howard']
+    //}
 
     const options = {
         legend: {
@@ -14,8 +35,8 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{ ...options, labels: mockData.labels }} //mock: usado quando se usa dados "de mentira", estáticos
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels }} //mock: usado quando se usa dados "de mentira", estáticos
+            series={chartData.series}
             type="donut"
             height="240"
         />
